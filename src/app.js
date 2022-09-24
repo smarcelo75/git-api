480415d26cc069d9fc0f6980876cfe7464768f5c
Marce Serrano <smarcelo75@hotmail.com>
2022-09-23 21:26:03 -0300

    feat: Servidor con Clases

--- src/app.js
+++ src/app.js
@@ -1,18 +1,5 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const portNumber = process.env.PORT;
+const Server = require('./models/server');
 
-app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-    res.json({
-        ok: true,
-        message: 'Hola mundo'
-    });
-});
-
-app.listen(portNumber, () => {
-    console.log(`app corriendo en puerto ${portNumber}`);
-});
+const server = new Server();
+server.listen();
